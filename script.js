4f0100d892afd2aa73f030723e7b2f09de175f4e
PrintFlow365 <clarionbd10@gmail.com>
2026-07-10 18:47:10 +0400

    Update script.js
    Load products from JSON file

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function changeLanguage(){
     alert("English version will be available soon.");
 
 }
+
+
+
+// Load Products
+
+fetch("products.json")
+.then(response => response.json())
+.then(products => {
+
+
+    console.log(products);
+
+
+})
+.catch(error => {
+
+    console.log("Product loading error:", error);
+
+});
