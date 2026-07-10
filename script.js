d8d694841f3abd938a9beed6f4f6c635ae7e1cc2
PrintFlow365 <clarionbd10@gmail.com>
2026-07-10 18:53:11 +0400

    Update script.js
    Display products from JSON

--- script.js
+++ script.js
@@ -9,16 +9,54 @@ function changeLanguage(){
 // Load Products
 
 fetch("products.json")
+
 .then(response => response.json())
+
 .then(products => {
 
 
-    console.log(products);
+    let container = document.getElementById("productContainer");
+
+
+    container.innerHTML = "";
+
+
+    products.forEach(product => {
+
+
+        let card = document.createElement("div");
+
+        card.className = "card";
+
+
+        card.innerHTML = `
+
+        <h3>${product.name_bn}</h3>
+
+        <p>${product.name_en}</p>
+
+        <p>${product.description_bn}</p>
+
+        <small>
+        ${product.category} - ${product.subcategory}
+        </small>
+
+        `;
+
+
+        container.appendChild(card);
+
+
+    });
 
 
 })
+
+
 .catch(error => {
 
+
     console.log("Product loading error:", error);
+
 
 });
