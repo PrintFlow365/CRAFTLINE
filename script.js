a58251679cf0eb0d82b63b3687b47d8a030460c4
PrintFlow365 <clarionbd10@gmail.com>
2026-07-12 01:18:00 +0400

    Update script.js
    Activate hero slider

--- script.js
+++ script.js
@@ -73,3 +73,23 @@ container.appendChild(card);
 
 
 });
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
