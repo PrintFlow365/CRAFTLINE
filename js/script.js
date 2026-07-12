3e11eeb96ebbf963f252396aa6da6300a48346b9
PrintFlow365 <clarionbd10@gmail.com>
2026-07-12 13:40:01 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -167,8 +167,3 @@ fetch("header.html")
 .then(data => {
     document.getElementById("header").innerHTML = data;
 });
-fetch("header.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("header").innerHTML = data;
-});
