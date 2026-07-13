d302c8b130725cc5c3d0313f5fa05e84eb036f65
PrintFlow365 <clarionbd10@gmail.com>
2026-07-13 15:26:52 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase-config.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
 async function testFirebase(){
