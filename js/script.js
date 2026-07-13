0b3b29a4af5476e1ee6f710074e7a3a02f00f120
PrintFlow365 <clarionbd10@gmail.com>
2026-07-13 12:41:22 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,60 +42,7 @@ if(orderBtn){
 }
 
 
-// Load Products
 
-fetch("data/products.json")
-
-.then(response => response.json())
-
-.then(products => {
-
-
-    let container = document.getElementById("productContainer");
-
-
-    container.innerHTML = "";
-
-
-    products.forEach(product => {
-
-
-        let card = document.createElement("div");
-
-        card.className = "card";
-
-
-        card.innerHTML = `
-
-<img src="images/${product.image}" alt="${product.name_en}" style="width:100%;max-height:200px;object-fit:contain;margin-bottom:10px;">
-
-<h3>${product.name_bn}</h3>
-
-<p>${product.name_en}</p>
-
-<p>${product.description_bn}</p>
-
-<small>
-${product.category} - ${product.subcategory}
-</small>
-
-`;
-container.appendChild(card);
-
-
-    });
-
-
-})
-
-
-.catch(error => {
-
-
-    console.log("Product loading error:", error);
-
-
-});
 document.addEventListener("DOMContentLoaded", function () {
 
     const slider = document.querySelector(".swiper");
